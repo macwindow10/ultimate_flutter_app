eece37fbffd7a7c4ee390dc46ad3668ec0fb621d
asif.raza@investmentlens.ca <asif.raza@investmentlens.ca>
2025-06-28 10:51:37 +0500

    initial

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.teal, brightness: Brightness.dark)),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Ultimate Flutter App')),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Person')
+          ],
+          selectedIndex: selectedIndex,
+        ),
+      ),
+    );
+  }
+}
