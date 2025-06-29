1acb8d3dc65940a6c2d65ebe93b6c53119da8da0
asif.raza@investmentlens.ca <asif.raza@investmentlens.ca>
2025-06-29 16:14:38 +0500

    lottie files added

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:ultimate_flutter_app/views/widget_tree.dart';
-import 'package:ultimate_flutter_app/views/widgets/hero_widget.dart';
+// import 'package:ultimate_flutter_app/views/widgets/hero_widget.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -11,7 +12,14 @@ class WelcomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          HeroWidget(),
+          // HeroWidget(),
+          Lottie.asset('assets/lotties/background_02.json',
+              height: 400, width: double.infinity),
+          Text(
+            'Ultimate Flutter App',
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 20.0),
+          ),
           FilledButton(
               onPressed: () {
                 Navigator.pushReplacement(context, MaterialPageRoute(
